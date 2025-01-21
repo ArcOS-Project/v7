8873ab75886fcf08905f7ad3cd7a9046746a35b8
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-01-21 12:52:06 +0100

    FUCKKKK

--- src/types/context.ts
+++ src/types/context.ts
@@ -1,6 +1,29 @@
+import type { App } from "./app";
+
 export interface ContextMenuItem {
-  label: string;
+  sep?: boolean;
+  caption?: string;
   icon?: string;
-  action?: () => void;
-  submenu?: ContextMenuItem[];
+  image?: string;
+  isActive?: ContextMenuCallback<boolean>;
+  action?: ContextMenuCallback;
+  subItems?: ContextMenuItem[];
+  disabled?: ContextMenuCallback<boolean>;
+  accelerator?: string;
 }
+
+export type ContextMenuCallback<T = void> = (
+  window: App | null,
+  data: DOMStringMap,
+  scope: string
+) => T | Promise<T>;
+
+export type ContextMenuInstance = null | {
+  x: number;
+  y: number;
+  items: ContextMenuItem[];
+  scope?: string;
+  scopeMap?: DOMStringMap;
+  app?: App;
+  artificial?: boolean;
+};
