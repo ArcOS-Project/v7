e868889647d1c8a113f9c07aec443494aa17490b
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-01-30 21:19:04 +0100

    network stuff in the tray

--- src/apps/components/shell/Shell/Taskbar/StatusTray/Network.svelte
+++ src/apps/components/shell/Shell/Taskbar/StatusTray/Network.svelte
@@ -4,16 +4,19 @@
   const { networkSpeed }: { networkSpeed: ReadableStore<number> } = $props();
 </script>
 
-{#if $networkSpeed > -1}
-  {#if $networkSpeed <= 10}
-    <span class="lucide icon-wifi-zero"></span>
-  {:else if $networkSpeed <= 35}
-    <span class="lucide icon-wifi-low"></span>
-  {:else if $networkSpeed <= 80}
-    <span class="lucide icon-wifi-high"></span>
+<div class="network">
+  <span class="lucide icon-wifi backdrop"></span>
+  {#if $networkSpeed > -1}
+    {#if $networkSpeed <= 10}
+      <span class="lucide icon-wifi-zero" title="Bad connection"></span>
+    {:else if $networkSpeed <= 35}
+      <span class="lucide icon-wifi-low" title="Medium connection"></span>
+    {:else if $networkSpeed <= 80}
+      <span class="lucide icon-wifi-high" title="Normal connection"></span>
+    {:else}
+      <span class="lucide icon-wifi" title="Good connection"></span>
+    {/if}
   {:else}
-    <span class="lucide icon-wifi"></span>
+    <span class="lucide icon-wifi-off" title="Unknown connection"></span>
   {/if}
-{:else}
-  <span class="lucide icon-wifi-off"></span>
-{/if}
+</div>
