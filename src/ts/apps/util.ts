d4ae4384c76a5ee099bbbb35da714c5376d6e33a
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-01-30 21:20:01 +0100

    Change type of isPopulatable to also include third party apps

--- src/ts/apps/util.ts
+++ src/ts/apps/util.ts
@@ -1,5 +1,5 @@
-import type { App } from "$types/app";
+import type { App, ThirdPartyApp } from "$types/app";
 
-export function isPopulatable(app: App) {
+export function isPopulatable(app: App | ThirdPartyApp) {
   return !app.hidden && !app.core && !app.overlay;
 }
