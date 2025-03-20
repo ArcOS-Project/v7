45d0b4b6599b41271aba1f411ccbccae5f424919
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-03-20 18:04:09 +0100

    Create the ArcTerm app proxy

---
+++ src/apps/user/arcterm/metadata.ts
@@ -0,0 +1,41 @@
+import { ArcTermIcon } from "$ts/images/apps";
+import type { App } from "$types/app";
+import { ArcTermRuntime } from "./process";
+
+export const ArcTermApp: App = {
+  metadata: {
+    name: "ArcTerm",
+    version: "4.0.0",
+    author: "Izaak Kuipers",
+    icon: ArcTermIcon,
+  },
+  size: {
+    w: 640,
+    h: 480,
+  },
+  minSize: {
+    w: 350,
+    h: 250,
+  },
+  maxSize: {
+    w: NaN,
+    h: NaN,
+  },
+  state: {
+    minimized: false,
+    maximized: false,
+    resizable: true,
+    headless: false,
+    fullscreen: false,
+  },
+  controls: {
+    minimize: true,
+    maximize: true,
+    close: true,
+  },
+  position: { centered: true },
+  assets: {
+    runtime: ArcTermRuntime,
+  },
+  id: "ArcTerm",
+};
---
+++ src/apps/user/arcterm/process.ts
@@ -0,0 +1,33 @@
+import { TerminalWindowRuntime } from "$apps/components/terminalwindow/runtime";
+import type { ProcessHandler } from "$ts/process/handler";
+import { Process } from "$ts/process/instance";
+import { UserDaemon } from "$ts/server/user/daemon";
+import { ArcTerminal } from "$ts/terminal";
+import type { AppProcessData } from "$types/app";
+
+export class ArcTermRuntime extends Process {
+  term: ArcTerminal | undefined;
+  path: string | undefined;
+  app: AppProcessData;
+
+  constructor(handler: ProcessHandler, pid: number, parentPid: number, app: AppProcessData, path?: string) {
+    super(handler, pid, parentPid);
+
+    this.path = path;
+    this.app = app;
+  }
+
+  protected async start(): Promise<any> {
+    const daemonPid = +this.env.get("userdaemon_pid");
+    const daemon = this.handler.getProcess<UserDaemon>(daemonPid);
+
+    if (!daemon) return false;
+
+    const proc = await daemon.spawnApp<TerminalWindowRuntime>("TerminalWindow", this.pid);
+
+    proc?.windowTitle.set("ArcTerm");
+    proc?.windowIcon.set(this.app.data.metadata.icon);
+
+    this.term = await this.handler.spawn<ArcTerminal>(ArcTerminal, daemon.getCurrentDesktop(), proc?.pid, proc?.term, this.path);
+  }
+}
