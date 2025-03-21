7dcd85d7e2449aa762098be08a28852f24c068a7
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-03-21 17:22:46 +0100

    Add color variables to terminal vars

--- src/ts/terminal/var/store.ts
+++ src/ts/terminal/var/store.ts
@@ -1,6 +1,26 @@
 import { ArcOSVersion } from "$ts/env";
 import type { VariableStore } from "$types/terminal";
 import type { ArcTerminal } from "..";
+import {
+  BLACK,
+  BLUE,
+  BRBLACK,
+  BRBLUE,
+  BRCYAN,
+  BRGREEN,
+  BRPURPLE,
+  BRRED,
+  BRWHITE,
+  BRYELLOW,
+  CYAN,
+  ESC,
+  GREEN,
+  PURPLE,
+  RED,
+  RESET,
+  WHITE,
+  YELLOW,
+} from "../store";
 
 export function getArcTermStore(term: ArcTerminal): VariableStore {
   return {
@@ -35,6 +55,97 @@ export function getArcTermStore(term: ArcTerminal): VariableStore {
       get: () => `${Math.floor(Math.random() * 1e6)}`,
       readOnly: true,
       canDelete: false,
+    },
+
+    ESC: {
+      get: () => ESC,
+      canDelete: false,
+      readOnly: true,
+    },
+    BLACK: {
+      get: () => BLACK,
+      canDelete: false,
+      readOnly: true,
+    },
+    RED: {
+      get: () => RED,
+      canDelete: false,
+      readOnly: true,
+    },
+    GREEN: {
+      get: () => GREEN,
+      canDelete: false,
+      readOnly: true,
+    },
+    YELLOW: {
+      get: () => YELLOW,
+      canDelete: false,
+      readOnly: true,
+    },
+    BLUE: {
+      get: () => BLUE,
+      canDelete: false,
+      readOnly: true,
+    },
+    PURPLE: {
+      get: () => PURPLE,
+      canDelete: false,
+      readOnly: true,
+    },
+    CYAN: {
+      get: () => CYAN,
+      canDelete: false,
+      readOnly: true,
+    },
+    WHITE: {
+      get: () => WHITE,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRBLACK: {
+      get: () => BRBLACK,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRRED: {
+      get: () => BRRED,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRGREEN: {
+      get: () => BRGREEN,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRYELLOW: {
+      get: () => BRYELLOW,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRBLUE: {
+      get: () => BRBLUE,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRPURPLE: {
+      get: () => BRPURPLE,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRCYAN: {
+      get: () => BRCYAN,
+      canDelete: false,
+      readOnly: true,
+    },
+    BRWHITE: {
+      get: () => BRWHITE,
+      canDelete: false,
+      readOnly: true,
+    },
+    RESET: {
+      get: () => RESET,
+      canDelete: false,
+      readOnly: true,
     },
   };
 }
