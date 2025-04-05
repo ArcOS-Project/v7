96fa84e3fd5336bfe041800497d93e06cb9bcf45
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-05 17:40:00 +0200

    ShareCreateGui

---
+++ src/apps/components/sharecreategui/ShareCreateGui.svelte
@@ -0,0 +1,34 @@
+<script lang="ts">
+  import InfoBlock from "$lib/InfoBlock.svelte";
+  import InfoRow from "$lib/InfoBlock/InfoRow.svelte";
+  import Segment from "$lib/InfoBlock/InfoRow/Segment.svelte";
+  import { ShareIcon } from "$ts/images/filesystem";
+  import type { ShareCreateGuiRuntime } from "./runtime";
+
+  const { process }: { process: ShareCreateGuiRuntime } = $props();
+  const { shareName, sharePassword } = process;
+</script>
+
+<div class="header">
+  <img src={ShareIcon} alt="" />
+  <h1>Create a share</h1>
+  <p>Fill out the fields to create a share. Each share is 512MB. You can create 5 shares in total.</p>
+</div>
+<InfoBlock>
+  <InfoRow>
+    <Segment title="Share name">
+      <input type="text" bind:value={$shareName} />
+    </Segment>
+  </InfoRow>
+  <InfoRow>
+    <Segment title="Share password">
+      <input type="password" bind:value={$sharePassword} />
+    </Segment>
+  </InfoRow>
+</InfoBlock>
+
+<div class="actions">
+  <button class="joined" onclick={() => process.myShares()}>My shares</button>
+  <button class="cancel" onclick={() => process.closeWindow()}>Cancel</button>
+  <button class="suggested" onclick={() => process.go()} disabled={!$shareName || !$sharePassword}>Create</button>
+</div>
---
+++ src/apps/components/sharecreategui/runtime.ts
@@ -0,0 +1,67 @@
+import { FileManagerRuntime } from "$apps/user/filemanager/runtime";
+import { AppProcess } from "$ts/apps/process";
+import { MessageBox } from "$ts/dialog";
+import type { ShareManager } from "$ts/fs/shares";
+import { ErrorIcon } from "$ts/images/dialog";
+import type { ProcessHandler } from "$ts/process/handler";
+import { Store } from "$ts/writable";
+import type { AppProcessData } from "$types/app";
+
+export class ShareCreateGuiRuntime extends AppProcess {
+  shareName = Store<string>();
+  sharePassword = Store<string>();
+  shares: ShareManager;
+
+  constructor(handler: ProcessHandler, pid: number, parentPid: number, app: AppProcessData) {
+    super(handler, pid, parentPid, app);
+
+    this.shares = this.userDaemon?.shares!;
+  }
+
+  async go() {
+    const name = this.shareName();
+    const password = this.sharePassword();
+
+    if (!name || !password) return;
+
+    const result = await this.shares.createShare(name, password);
+
+    if (!result) {
+      MessageBox(
+        {
+          title: "Failed to create share",
+          message:
+            "ArcOS was unable to create the share you requested. You might already have the maximum amount of shares in your account.",
+          buttons: [{ caption: "Okay", action: () => {}, suggested: true }],
+          image: ErrorIcon,
+          sound: "arcos.dialog.error",
+        },
+        this.pid,
+        true
+      );
+
+      return;
+    }
+
+    const drive = await this.shares.mountShareById(result._id);
+
+    if (!drive) return;
+
+    const path = `${drive.uuid}:/`;
+    const parent = this.handler.getProcess(this.parentPid);
+
+    if (parent && parent instanceof FileManagerRuntime) {
+      const dispatch = this.handler.ConnectDispatch(this.parentPid);
+      dispatch?.dispatch("navigate", path);
+    } else {
+      this.spawnApp("fileManager", +this.env.get("shell_pid"), path);
+    }
+
+    this.closeWindow();
+  }
+
+  async myShares() {
+    await this.closeWindow();
+    this.spawnOverlayApp("ShareListGui", this.parentPid);
+  }
+}
