d77e5c3d20a66b6d43d0e2f9ef43ffab8a044b96
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-12 18:18:12 +0200

    Add not implemented to bug hunt action bar

--- src/apps/user/bughunt/BugHunt/ActionBar.svelte
+++ src/apps/user/bughunt/BugHunt/ActionBar.svelte
@@ -12,11 +12,27 @@
   </button>
   {#if report}
     <div class="actions">
-      <button class="lucide icon-scroll-text" aria-label="View logs" disabled={!report.logs.length}></button>
-      <button class="lucide icon-braces" aria-label="View user data" disabled={!report.userData}></button>
-      <button class="lucide icon-file-text" aria-label="View body" disabled={!report.body}></button>
+      <button
+        class="lucide icon-scroll-text"
+        aria-label="View logs"
+        disabled={!report.logs.length}
+        onclick={() => process.notImplemented("Viewing report logs")}
+      ></button>
+      <button
+        class="lucide icon-braces"
+        aria-label="View user data"
+        disabled={!report.userData}
+        onclick={() => process.notImplemented("Viewing user data")}
+      ></button>
+      <button
+        class="lucide icon-file-text"
+        aria-label="View body"
+        disabled={!report.body}
+        onclick={() => process.notImplemented("Viewing report body")}
+      ></button>
       <div class="sep"></div>
-      <button class="lucide icon-save" aria-label="Save JSON"></button>
+      <button class="lucide icon-save" aria-label="Save JSON" onclick={() => process.notImplemented("Exporting reports")}
+      ></button>
       <button class="lucide icon-refresh-cw" aria-label="Refresh" onclick={() => process.invalidateCaches(true)}></button>
     </div>
   {/if}
