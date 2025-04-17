746b29d51da103a89dfa355891c41414282886f3
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-18 01:49:37 +0200

    some minor crash improvements

--- src/ts/kernel/premature.ts
+++ src/ts/kernel/premature.ts
@@ -10,13 +10,22 @@ export function prematurePanic() {
   const icon = document.createElement("span");
   const header = document.createElement("h1");
   const text = document.createElement("p");
+  const shortcutHint = document.createElement("div");
+  const shortcutHintIcon = document.createElement("span");
+  const shortcutHintText = document.createElement("span");
 
-  icon.className = "lucide icon-ghost";
+  icon.className = "lucide icon-hand";
   text.innerText = "arcos.izkuipers.nl/support";
-  header.innerText = "Kernel init failed";
+  header.innerText = "The kernel failed prematurely";
+
+  shortcutHint.className = "shortcut-hint";
+  shortcutHintIcon.className = "lucide icon-rotate-cw";
+  shortcutHintText.innerText = "Ctrl+R";
+
+  shortcutHint.append(shortcutHintIcon, shortcutHintText);
 
   wrapper.id = "crashScreen";
-  wrapper.append(icon, header, text);
+  wrapper.append(icon, header, text, shortcutHint);
 
   main.append(wrapper);
 }
