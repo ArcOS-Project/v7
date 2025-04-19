799d5255553abf0fcf648790c56c07ea5af01853
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-19 17:16:12 +0200

    Disable soundbus in safemode

--- src/ts/soundbus/index.ts
+++ src/ts/soundbus/index.ts
@@ -1,4 +1,5 @@
 import type { WaveKernel } from "$ts/kernel";
+import { Environment } from "$ts/kernel/env";
 import { KernelModule } from "$ts/kernel/module";
 import type { SoundBusStore, SoundStore } from "$types/soundbus";
 import { ArcSounds } from "./store";
@@ -6,21 +7,21 @@ import { ArcSounds } from "./store";
 export class SoundBus extends KernelModule {
   private store: SoundStore = {};
   private _bus: SoundBusStore = {};
+  private env: Environment;
 
   constructor(kernel: WaveKernel, id: string) {
     super(kernel, id);
 
     this.store = ArcSounds;
+    this.env = kernel.getModule<Environment>("env");
   }
 
   public playSound(id: string, volume = 1) {
     if (!this.IS_KMOD) throw new Error("Not a kernel module");
-
+    if (this.env.get("safemode")) return;
     if (!this.store[id]) return false;
 
     this.Log(`Playing sound ${id} from store`);
-
-    // TODO: SafeMode in v7
 
     const element = document.createElement("audio");
 
