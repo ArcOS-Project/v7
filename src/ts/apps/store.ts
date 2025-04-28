f1fbb54b5863552fdb8afa157101f5b27b8f2e89
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-28 17:38:05 +0200

    Load ContextMenuApp, SystemShortcuts

--- src/ts/apps/store.ts
+++ src/ts/apps/store.ts
@@ -1,6 +1,7 @@
 import { AdminPortalApp } from "$apps/admin/adminportal/metadata";
 import { AcceleratorOverviewApp } from "$apps/components/acceleratoroverview/metadata";
 import { AppInfoApp } from "$apps/components/appinfo/metadata";
+import { ContextMenuApp } from "$apps/components/contextmenu/metadata";
 import { ExitApp } from "$apps/components/exit/metadata";
 import { FsNewFileApp } from "$apps/components/fsnewfile/metadata";
 import { FsNewFolderApp } from "$apps/components/fsnewfolder/metadata";
@@ -20,6 +21,7 @@ import { ShareMgmtGuiApp } from "$apps/components/sharemgmtgui/metadata";
 import { ArcShellApp } from "$apps/components/shell/metadata";
 import { ShellHostApp } from "$apps/components/shellhost/metadata";
 import { ShortcutPropertiesApp } from "$apps/components/shortcutproperties/metadata";
+import { SystemShortcuts } from "$apps/components/systemshortcuts/metadata";
 import { TerminalWindowApp } from "$apps/components/terminalwindow/metadata";
 import { TotpAuthGuiApp } from "$apps/components/totpauthgui/metadata";
 import { TotpSetupGuiApp } from "$apps/components/totpsetupgui/metadata";
@@ -49,6 +51,8 @@ import type { AppKeyCombinations } from "$types/accelerator";
 import type { AppStorage } from "$types/app";
 
 export const BuiltinApps: AppStorage = [
+  ContextMenuApp,
+  SystemShortcuts,
   AppInfoApp,
   FsProgressApp,
   IconPickerApp,
