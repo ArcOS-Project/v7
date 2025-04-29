d0916b276b1398630665509a20eef5399f5f4312
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-29 20:05:05 +0200

    Create ViewBugReport.svelte

---
+++ src/apps/admin/adminportal/AdminPortal/Page/ViewBugReport.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import type { AdminPortalRuntime } from "../../runtime";
+  import type { ViewBugReportData } from "../../types";
+  import Header from "./ViewBugReport/Header.svelte";
+  import LeftPanel from "./ViewBugReport/LeftPanel.svelte";
+  import RightPanel from "./ViewBugReport/RightPanel.svelte";
+
+  const { process, data }: { process: AdminPortalRuntime; data: ViewBugReportData } = $props();
+  const { report } = data;
+</script>
+
+<Header {data} {process} />
+<div class="panels">
+  <LeftPanel {report} />
+  <RightPanel {report} />
+</div>
