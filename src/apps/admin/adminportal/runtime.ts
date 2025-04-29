23d0d5d84a45266118046d53f01ecbb0b957aad6
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-29 20:06:22 +0200

    admin portal: Add switchPage props and force bool

--- src/apps/admin/adminportal/runtime.ts
+++ src/apps/admin/adminportal/runtime.ts
@@ -1,14 +1,20 @@
 import { AppProcess } from "$ts/apps/process";
-import { ElevationIcon } from "$ts/images/general";
 import type { ProcessHandler } from "$ts/process/handler";
 import { AdminBootstrapper } from "$ts/server/admin";
+import { Sleep } from "$ts/sleep";
 import { Store } from "$ts/writable";
-import type { AppProcessData } from "$types/app";
+import type { App, AppProcessData } from "$types/app";
 import { AdminPortalPageStore } from "./store";
+import { BugHuntUserDataApp } from "./userdata/metadata";
 
 export class AdminPortalRuntime extends AppProcess {
   currentPage = Store<string>("");
+  switchPageProps = Store<Record<string, any>>({});
   admin: AdminBootstrapper;
+
+  protected overlayStore: Record<string, App> = {
+    userdata: BugHuntUserDataApp,
+  };
 
   constructor(handler: ProcessHandler, pid: number, parentPid: number, app: AppProcessData, page?: string) {
     super(handler, pid, parentPid, app);
@@ -17,13 +23,18 @@ export class AdminPortalRuntime extends AppProcess {
     this.switchPage(page || "dashboard");
   }
 
-  switchPage(pageId: string) {
+  async switchPage(pageId: string, props: Record<string, any> = {}, force = false) {
     this.Log(`Loading page '${pageId}'`);
 
     if (!AdminPortalPageStore.has(pageId)) return;
 
     const page = AdminPortalPageStore.get(pageId);
 
+    this.switchPageProps.set(props);
+    if (force) {
+      this.currentPage.set("");
+      await Sleep(0);
+    }
     this.currentPage.set(pageId);
 
     this.windowTitle.set(`${page?.name} - Admin Portal`);
