a1f1af70ed98d5e8e3549e06344160455976a236
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-30 15:48:07 +0200

    Change bug report rows to selection

--- src/apps/admin/adminportal/AdminPortal/Page/BugHunt/Row.svelte
+++ src/apps/admin/adminportal/AdminPortal/Page/BugHunt/Row.svelte
@@ -2,18 +2,29 @@
   import type { AdminPortalRuntime } from "$apps/admin/adminportal/runtime";
   import { LogoTranslations } from "$apps/admin/adminportal/store";
   import { QuestionIcon } from "$ts/images/dialog";
+  import type { ReadableStore } from "$ts/writable";
   import type { User } from "$types/admin";
   import type { BugReport } from "$types/bughunt";
   import dayjs from "dayjs";
 
-  const { process, report, users }: { process: AdminPortalRuntime; report: BugReport; users: User[] } = $props();
+  const {
+    process,
+    report,
+    users,
+    idEntry,
+  }: { process: AdminPortalRuntime; report: BugReport; users: User[]; idEntry: ReadableStore<string> } = $props();
   const timestamp = dayjs(report.createdAt).format("D MMM YYYY, HH:mm");
   const author = users.filter((u) => u._id === report.authorId)[0];
 </script>
 
 <!-- svelte-ignore a11y_click_events_have_key_events -->
 <!-- svelte-ignore a11y_no_static_element_interactions -->
-<div class="row" onclick={() => process.switchPage("viewBugReport", { report })}>
+<div
+  class="row"
+  onclick={() => ($idEntry = report._id!)}
+  class:selected={$idEntry === report._id}
+  ondblclick={() => process.switchPage("viewBugReport", { report })}
+>
   <div class="segment mode-icon"><img src={LogoTranslations[report.mode] || QuestionIcon} alt="" /></div>
   <div class="segment timestamp">{timestamp}</div>
   <div class="segment title">{report.title}</div>
