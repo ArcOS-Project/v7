92d3cb2e4ffbff32986f7cf86f3367e32549ec5c
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-04-30 15:47:14 +0200

    Add id entry to bug hunt page

--- src/apps/admin/adminportal/AdminPortal/Page/BugHunt.svelte
+++ src/apps/admin/adminportal/AdminPortal/Page/BugHunt.svelte
@@ -6,16 +6,19 @@
   import type { BugHuntData } from "../../types";
   import Row from "./BugHunt/Row.svelte";
   import { Logo } from "$ts/branding";
+  import { MessageBox } from "$ts/dialog";
+  import { ErrorIcon } from "$ts/images/dialog";
 
   const { process, data }: { process: AdminPortalRuntime; data: BugHuntData } = $props();
   const { users, reports, stats } = data;
   const pages: ("all" | "opened" | "closed" | "resolved")[] = ["all", "opened", "closed", "resolved"];
 
   let store = Store<BugReport[]>([]);
-  let state = Store<"all" | "opened" | "closed" | "resolved">("all");
+  let sortState = Store<"all" | "opened" | "closed" | "resolved">("all");
+  let idEntry = Store("");
 
   onMount(() => {
-    state.subscribe((v) => {
+    sortState.subscribe((v) => {
       $store = reports
         .filter((report) => {
           switch (v) {
@@ -32,13 +35,34 @@
         .reverse();
     });
   });
+
+  function useIdEntry() {
+    const report = reports.filter((r) => r._id === $idEntry)[0];
+
+    if (!report) {
+      MessageBox(
+        {
+          title: "Report ID not found",
+          message: "Sorry, the report ID you entered doesn't match any records. Check it, and then try again.",
+          buttons: [{ caption: "Okay", action: () => {}, suggested: true }],
+          image: ErrorIcon,
+          sound: "arcos.dialog.error",
+        },
+        process.pid,
+        true,
+      );
+      return;
+    }
+
+    process.switchPage("viewBugReport", { report });
+  }
 </script>
 
 <div class="list-wrapper">
   <div class="tabs">
-    <p>{$state} ({$store.length})</p>
+    <p>{$sortState} ({$store.length})</p>
     {#each pages as page}
-      <button class:selected={page === $state} onclick={() => ($state = page)}>{page.toUpperCase()}</button>
+      <button class:selected={page === $sortState} onclick={() => ($sortState = page)}>{page.toUpperCase()}</button>
     {/each}
   </div>
   <div class="listing">
@@ -49,11 +73,18 @@
       <div class="segment author">Author</div>
     </div>
     {#each $store as report (report._id)}
-      <Row {process} {report} {users} />
+      <Row {process} {report} {users} {idEntry} />
     {/each}
     {#if !$store.length}
       <p class="no-results">No results.</p>
     {/if}
+  </div>
+  <div class="id-entry">
+    <div class="icon">
+      <span class="lucide icon-rectangle-ellipsis"></span>
+    </div>
+    <input type="text" placeholder="Enter ID..." bind:value={$idEntry} maxlength="24" />
+    <button disabled={$idEntry.length !== 24} onclick={useIdEntry}>Go</button>
   </div>
 </div>
 <div class="stats">
