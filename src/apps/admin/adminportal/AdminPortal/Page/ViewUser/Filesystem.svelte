ef94e2f29eeb5f427967434fd31633c82aca18ec
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-05-02 18:39:50 +0200

    code cleanup

---
+++ src/apps/admin/adminportal/AdminPortal/Page/ViewUser/Filesystem.svelte
@@ -0,0 +1,36 @@
+<script lang="ts">
+  import type { AdminPortalRuntime } from "$apps/admin/adminportal/runtime";
+  import CircularProgress from "$lib/CircularProgress.svelte";
+  import Spinner from "$lib/Spinner.svelte";
+  import { formatBytes } from "$ts/fs/util";
+  import type { UserQuota } from "$types/fs";
+  import type { ExpandedUserInfo } from "$types/user";
+  import { onMount } from "svelte";
+
+  const { process, user }: { process: AdminPortalRuntime; user: ExpandedUserInfo } = $props();
+
+  let quota: UserQuota | undefined = $state();
+  let loading = $state(true);
+
+  onMount(async () => {
+    quota = await process.admin.getQuotaOf(user.username);
+    loading = false;
+  });
+</script>
+
+<div class="section quota">
+  <h1>Filesystem</h1>
+  <div>
+    {#if loading}
+      <Spinner height={16} />
+    {:else if quota}
+      <CircularProgress size={48} strokeWidth={5} max={quota.max} value={quota.used} />
+      <div>
+        <p class="used">{formatBytes(quota.used)} / {formatBytes(quota.max)}</p>
+        <p class="percentage">({quota.percentage.toFixed(2)}%)</p>
+      </div>
+    {:else}
+      <p class="error-text">Failed to get quota</p>
+    {/if}
+  </div>
+</div>
---
+++ src/apps/admin/adminportal/AdminPortal/Page/ViewUser/Identity.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+  import type { ExpandedUserInfo } from "$types/user";
+
+  const { user }: { user: ExpandedUserInfo } = $props();
+</script>
+
+<div class="identity">
+  <img src={user.profile.profilePicture} alt="" />
+  <div>
+    <h1>{user.profile.displayName || user.profile.username}</h1>
+    <p>{user.email} - {user.username}</p>
+    <div class="badges">
+      {#if user.admin}
+        <div class="badge admin">Admin</div>
+      {/if}
+      {#if user.approved}
+        <div class="badge approved">Approved</div>
+      {/if}
+      <div class="badge account-number">#{user.accountNumber}</div>
+    </div>
+  </div>
+</div>
