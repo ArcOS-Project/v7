9ad4c4cd09412a457d3776f92b94fb9af6de3864
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-05-03 21:38:00 +0200

    add markdown support to messagecomposer

--- src/apps/components/messagecomposer/MessageComposer.svelte
+++ src/apps/components/messagecomposer/MessageComposer.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+  import MarkdownEditorComponent from "$lib/MarkdownEditorComponent.svelte";
   import ActionBar from "./MessageComposer/ActionBar.svelte";
   import AttachmentBar from "./MessageComposer/AttachmentBar.svelte";
   import SubjectField from "./MessageComposer/SubjectField.svelte";
@@ -14,7 +15,7 @@
   <SubjectField {process} />
 </div>
 <div class="message-body">
-  <textarea name="" id="" bind:value={$body} disabled={$sending}></textarea>
+  <MarkdownEditorComponent bind:value={$body} />
 </div>
 <AttachmentBar {process} />
 <ActionBar {process} />
