0f64001d303fccc0f048bf2a09e9d97935f0a5af
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-05-06 15:22:27 +0200

    Continue working on ViewUser

--- src/apps/admin/adminportal/AdminPortal/Page/ViewUser.svelte
+++ src/apps/admin/adminportal/AdminPortal/Page/ViewUser.svelte
@@ -24,16 +24,49 @@
   <Shares {user} {process} />
 </div>
 <div class="rightpanel">
-  {#if statistics}
-    <div class="statistics">
+  <div class="statistics">
+    {#if statistics}
       {#each Object.entries(statistics) as [what, count]}
         <div class="statistic">
           <h1>{what}</h1>
           <p class="big-value">{count}</p>
         </div>
       {/each}
+    {:else}
+      <Spinner height={32} />
+    {/if}
+  </div>
+  <div class="quick-actions">
+    <button>Log out</button>
+    <button>Disapprove</button>
+    <button>Grant Admin</button>
+  </div>
+  <div class="section username">
+    <h1>Rename user</h1>
+    <div>
+      <input type="text" placeholder={user.username} />
+      <div class="buttons">
+        <button class="suggested">Change</button>
+      </div>
     </div>
-  {:else}
-    <Spinner height={32} />
-  {/if}
+  </div>
+  <div class="section email">
+    <h1>Change email</h1>
+    <div>
+      <input type="email" placeholder={user.email} />
+      <div class="buttons">
+        <button class="suggested">Change</button>
+      </div>
+    </div>
+  </div>
+  <div class="section password">
+    <h1>Change password</h1>
+    <div>
+      <input type="password" />
+      <div class="buttons">
+        <button>Generate</button>
+        <button class="suggested">Change</button>
+      </div>
+    </div>
+  </div>
 </div>
