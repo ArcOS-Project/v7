5560a4f8384bdadde54005ced65d6a8eedd9db2a
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-05-15 10:41:30 +0200

    Okay fuck Carta for a minute

--- src/lib/MarkdownRenderingComponent.svelte
+++ src/lib/MarkdownRenderingComponent.svelte
@@ -1,14 +1,9 @@
 <script lang="ts">
-  import { Carta, Markdown } from "carta-md";
+  import SvelteMarkdown from "svelte-markdown";
 
   const { source }: { source: string } = $props();
-  const carta = new Carta({
-    sanitizer: false,
-    extensions: [],
-    shikiOptions: {},
-  });
 </script>
 
 <div class="markdown-body">
-  <Markdown value={source} {carta} />
+  <SvelteMarkdown {source} />
 </div>
