87e19eb9e2e0d0c808072e4c817b2a7981877fc4
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-05-21 18:55:16 +0200

    Inline the types of Writables into ts/writables.ts

--- src/ts/writable.ts
+++ src/ts/writable.ts
@@ -1,4 +1,37 @@
-import { get, writable, type Writable } from "svelte/store";
+import { get, writable } from "svelte/store";
+
+/** Callback to inform of a value updates. */
+export type Subscriber<T> = (value: T) => void;
+
+/** Unsubscribes from value updates. */
+export type Unsubscriber = () => void;
+
+/** Callback to update a value. */
+export type Updater<T> = (value: T) => T;
+/** Readable interface for subscribing. */
+export interface Readable<T> {
+  /**
+   * Subscribe on value changes.
+   * @param run subscription callback
+   * @param invalidate cleanup callback
+   */
+  subscribe(this: void, run: Subscriber<T>, invalidate?: () => void): Unsubscriber;
+}
+
+/** Writable interface for both updating and subscribing. */
+export interface Writable<T> extends Readable<T> {
+  /**
+   * Set value and inform subscribers.
+   * @param value to set
+   */
+  set(this: void, value: T): void;
+
+  /**
+   * Update value using callback and inform subscribers.
+   * @param updater callback
+   */
+  update(this: void, updater: Updater<T>): void;
+}
 
 export type ReadableStore<T> = Writable<T> & { (): T; get: () => T };
 export type BooleanStore = ReadableStore<boolean>;
