280eec312c1412bc2f064808a4062e568d9c93d8
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-05-30 17:30:27 +0200

    Finish dispatch clients page in AdvSysSet

--- src/apps/user/advsystemsettings/AdvancedSystemSettings/DispatchClients.svelte
+++ src/apps/user/advsystemsettings/AdvancedSystemSettings/DispatchClients.svelte
@@ -2,15 +2,19 @@
   import type { GlobalDispatchClient } from "$types/dispatch";
   import { onMount } from "svelte";
   import type { AdvSysSetRuntime } from "../runtime";
+  import Spinner from "$lib/Spinner.svelte";
 
   const { process }: { process: AdvSysSetRuntime } = $props();
 
   let clients = $state<GlobalDispatchClient[]>([]);
+  let loading = $state<boolean>(false);
 
   onMount(update);
 
   async function update() {
+    loading = true;
     clients = await process.userDaemon!.globalDispatch!.getClients();
+    loading = false;
   }
 
   async function disconnectClient(clientId: string) {
@@ -19,17 +23,34 @@
   }
 </script>
 
-<p>Below follows a list of Global Dispatch clients currently connected to your account.</p>
+<p>
+  Below is a list of Global Dispatch clients currently connected to your account. You can disconnect one by clicking the button.
+</p>
 <div class="list">
-  <div class="row head">
-    <div class="segment ip">IP address</div>
-    <div class="segment authorized">Authorized</div>
-  </div>
-  {#each clients as client (client.socketId)}
-    <div class="row" title={client.socketId}>
-      <div class="segment ip">{client.ip}</div>
-      <div class="segment authorized">{client.authorized ? "Yes" : "No"}</div>
-      <button class="lucide icon-link-2-off" onclick={() => disconnectClient(client.socketId)} aria-label="Kick client"></button>
+  {#if !loading}
+    <div class="row head">
+      <div class="segment ip">IP address</div>
+      <div class="segment authorized">Authorized</div>
+      <button class="lucide icon-rotate-cw" disabled={loading} aria-label="Refresh" onclick={update}></button>
     </div>
-  {/each}
+    {#each clients as client (client.socketId)}
+      <div class="row" title={client.socketId}>
+        <div class="segment ip">
+          {client.ip}
+          {#if client.socketId === process.userDaemon?.globalDispatch?.client?.id}(you){/if}
+        </div>
+        <div class="segment authorized">{client.authorized ? "Yes" : "No"}</div>
+        <button
+          class="lucide icon-link-2-off"
+          onclick={() => disconnectClient(client.socketId)}
+          aria-label="Kick client"
+          disabled={client.socketId === process.userDaemon?.globalDispatch?.client?.id}
+        ></button>
+      </div>
+    {/each}
+  {:else}
+    <div class="loading">
+      <Spinner height={32} />
+    </div>
+  {/if}
 </div>
