c44cec78e0ae9365b8a9dfd6f3c43f350b8c4453
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-06-01 15:21:45 +0200

    Try to close the window in turnedoff

--- src/state/turnedoff/render.ts
+++ src/state/turnedoff/render.ts
@@ -4,4 +4,10 @@ export default async function TurnedOff() {
   const img = document.querySelector("#safeToTurnOff") as HTMLImageElement;
 
   if (img) img.src = safeToTurnOff;
+
+  try {
+    window.close();
+  } catch {
+    // Silently error
+  }
 }
