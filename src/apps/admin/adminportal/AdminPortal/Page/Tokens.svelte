8c430e33c09749d9211134bc3fb8f2aed8aeb6a5
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-06-12 12:31:40 +0200

    AP: Tokens page

--- src/apps/admin/adminportal/AdminPortal/Page/Tokens.svelte
+++ src/apps/admin/adminportal/AdminPortal/Page/Tokens.svelte
@@ -1,1 +1,28 @@
-tokens
+<script lang="ts">
+  import { sortByKey } from "$ts/util";
+  import { Store } from "$ts/writable";
+  import type { AdminPortalRuntime } from "../../runtime";
+  import type { TokensData } from "../../types";
+  import TokenRow from "./Tokens/TokenRow.svelte";
+
+  const { process, data }: { process: AdminPortalRuntime; data: TokensData } = $props();
+  const { users } = data;
+  const sortMode = Store<string>("lastUsed");
+</script>
+
+<div class="token-list">
+  <div class="row header">
+    <div class="segment icon">
+      <span class="lucide icon-key"></span>
+    </div>
+    <div class="segment author">Username</div>
+    <div class="segment browser">Browser</div>
+    <div class="segment os">OS</div>
+    <div class="segment last-used"><button class="sort-mode" onclick={() => ($sortMode = "lastUsed")}>Last used</button></div>
+    <div class="segment times-used"><button class="sort-mode" onclick={() => ($sortMode = "timesUsed")}>Times used</button></div>
+    <div class="segment actions">Actions</div>
+  </div>
+  {#each sortByKey( data.tokens.map( (t) => ({ ...t, user: users.filter((u) => u._id === t.userId)[0] }), ), $sortMode, true, ) as token (token._id)}
+    <TokenRow {token} {process} />
+  {/each}
+</div>
---
+++ src/apps/admin/adminportal/AdminPortal/Page/Tokens/TokenRow.svelte
@@ -0,0 +1,80 @@
+<script lang="ts">
+  import type { AdminPortalRuntime } from "$apps/admin/adminportal/runtime";
+  import ProfilePicture from "$lib/ProfilePicture.svelte";
+  import { MessageBox } from "$ts/dialog";
+  import { ElevationIcon } from "$ts/images/general";
+  import type { ExpandedToken } from "$types/admin";
+  import dayjs from "dayjs";
+  import { UAParser } from "ua-parser-js";
+  import Cookies from "js-cookie";
+  import { Sleep } from "$ts/sleep";
+
+  const { token, process }: { token: ExpandedToken; process: AdminPortalRuntime } = $props();
+  const parsed = UAParser(token.userAgent);
+  const lastUsed = dayjs(token.lastUsed).format("D MMM YYYY, HH:mm");
+
+  async function useToken() {
+    MessageBox(
+      {
+        title: "Are you sure?",
+        message:
+          "This replaces your current token with this user's token, causing you to log in as this user. You really should NEVER do this unless you have some justifiable reason.",
+        buttons: [
+          { caption: "Cancel", action: () => {} },
+          {
+            caption: "Log In As",
+            suggested: true,
+            action: async () => {
+              await process.userDaemon?.logoff();
+              await Sleep(3000);
+              const cookieOptions = {
+                expires: 2,
+                domain: import.meta.env.DEV ? "localhost" : "izkuipers.nl",
+              };
+              Cookies.set("arcToken", token.value, cookieOptions);
+              Cookies.set("arcUsername", token.user?.username!, cookieOptions);
+              process.userDaemon!._disposed = false;
+              await process.userDaemon?.restart();
+            },
+          },
+        ],
+        image: ElevationIcon,
+        sound: "arcos.dialog.warning",
+      },
+      process.pid,
+      true,
+    );
+  }
+</script>
+
+<div class="row">
+  <div class="segment icon">
+    <span class="lucide icon-key"></span>
+  </div>
+  <div class="segment author">
+    {#if token.user}
+      <ProfilePicture height={20} fallback={token.user.profile.profilePicture} />
+      <button class="link" onclick={() => process.switchPage("viewUser", { user: token.user })}>{token.user.username}</button>
+    {:else}
+      Stranger
+    {/if}
+  </div>
+  <div class="segment browser">
+    {parsed.browser.name}
+    {parsed.browser.version}
+  </div>
+  <div class="segment os">
+    {parsed.os.name}
+    {parsed.os.version}
+  </div>
+  <div class="segment last-used">
+    {lastUsed}
+  </div>
+  <div class="segment times-used">
+    {token.timesUsed}
+  </div>
+  <div class="segment actions">
+    <button class="delete clr-orange" onclick={useToken}>Use...</button>
+    <button class="delete clr-red">Delete</button>
+  </div>
+</div>
