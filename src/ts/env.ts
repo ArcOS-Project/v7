a5e0445d4bd86c8b0fd30cc28f17a5045cbd51a3
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-06-15 23:01:10 +0200

    Bump to 7.0.3!

--- src/ts/env.ts
+++ src/ts/env.ts
@@ -1,3 +1,3 @@
-export const ArcOSVersion = "7.0.2";
+export const ArcOSVersion = "7.0.3";
 export const VALIDATION_STR = "thisWonderfulArcOSServerIdentifiedByTheseWordsPleaseDontSteal(c)IzKuipers";
 export const BETA = true;
