2c7d365964a05c342fa9c278c4b7ce0ee67cf99b
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-06-27 17:48:03 +0200

    everything

--- src/apps/user/appstore/Pages/Everything.svelte
+++ src/apps/user/appstore/Pages/Everything.svelte
@@ -1,1 +1,10 @@
-everything
+<script lang="ts">
+  import type { PartialStoreItem, StoreItem } from "$types/package";
+  import PackageGrid from "../AppStore/PackageGrid.svelte";
+  import type { AppStoreRuntime } from "../runtime";
+
+  const { all, process }: { all: PartialStoreItem[]; process: AppStoreRuntime } = $props();
+</script>
+
+<PackageGrid items={all} name="Everything" {process} />
+<p class="end">Looks like you've reached the end.</p>
