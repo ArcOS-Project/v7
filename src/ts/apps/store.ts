efa61b3c58a449437643759f177db8d4bd9603c5
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-07-04 23:31:11 +0200

    Load MultiUpdateGui

--- src/ts/apps/store.ts
+++ src/ts/apps/store.ts
@@ -15,6 +15,7 @@ import { IconPickerApp } from "$apps/components/iconpicker/metadata";
 import { ItemInfoApp } from "$apps/components/iteminfo/metadata";
 import { MessageBoxApp } from "$apps/components/messagebox/metadata";
 import { MessageComposerApp } from "$apps/components/messagecomposer/metadata";
+import { MultiUpdateGuiApp } from "$apps/components/multiupdategui/metadata";
 import { OpenWithApp } from "$apps/components/openwith/metadata";
 import { SecureContextApp } from "$apps/components/securecontext/metadata";
 import { ShareConnGuiApp } from "$apps/components/shareconngui/metadata";
@@ -106,6 +107,7 @@ export const BuiltinApps: AppStorage = [
   AppInstallerApp,
   ArcFind,
   AppStoreApp,
+  MultiUpdateGuiApp,
 ];
 
 export const AdminApps: AppStorage = [AdminPortalApp];
