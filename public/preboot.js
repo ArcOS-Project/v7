9249ea2db147d71c6e0fa7d73836a39551a1b1bc
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-07-07 01:37:17 +0200

    PreBoot SystemLoadFail

---
+++ public/preboot.js
@@ -0,0 +1,27 @@
+(() => {
+  function loadFail(r) {
+    const url = "https://docs.arcapi.nl/reference/troubleshooting#the-dots-of-doom";
+    const pre = document.createElement("pre");
+
+    pre.innerHTML = `Unable to load ArcOS: your browser might not be compatible.<br><br>`;
+    pre.innerHTML += r;
+    pre.innerHTML += `<br><br><a class="troubleshooting" href="${url}" target="_blank">${url}</a>`;
+    pre.className = "system-load-fail";
+    document.querySelector("#stateLoader")?.append(pre);
+    document.body.classList.add("slf");
+  }
+
+  const event = (e) => {
+    if (window.__DW_INIT__) return;
+
+    if (e instanceof ErrorEvent) {
+      loadFail(e.error.stack);
+      console.warn(`${e.error.stack}`);
+    } else if (e instanceof PromiseRejectionEvent) {
+      console.warn(`${e.reason}`);
+    }
+  };
+
+  window.addEventListener("unhandledrejection", event, { once: true });
+  window.addEventListener("error", event, { once: true });
+})();
