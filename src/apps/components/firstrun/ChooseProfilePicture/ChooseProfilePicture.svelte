9162c0f3b9cefd7990ef3dced7ac4b634623993f
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-08-07 23:52:22 +0200

    FirstRun_ChooseProfilePicture

---
+++ src/apps/components/firstrun/ChooseProfilePicture/ChooseProfilePicture.svelte
@@ -0,0 +1,27 @@
+<script lang="ts">
+  import ProfilePicture from "$lib/ProfilePicture.svelte";
+  import type { AppProcess } from "$ts/apps/process";
+  import { ProfilePictures } from "$ts/images/pfp";
+
+  const { process }: { process: AppProcess } = $props();
+  const { userPreferences, userDaemon } = process;
+</script>
+
+<div class="top">
+  <h1>Choose profile picture</h1>
+  <p>What do you want to be?</p>
+  <div class="profile-picture-grid">
+    {#each Object.values(ProfilePictures) as pfp, i}
+      <button
+        class="picture-option"
+        onclick={() => process.userDaemon?.changeProfilePicture(i + 1)}
+        class:selected={$userPreferences.account.profilePicture === i + 1}
+      >
+        <ProfilePicture {userDaemon} fallback={pfp} height={40} />
+      </button>
+    {/each}
+  </div>
+</div>
+<div class="actions">
+  <button class="suggested" onclick={() => process.closeWindow()}>Close</button>
+</div>
---
+++ src/apps/components/firstrun/ChooseProfilePicture/metadata.ts
@@ -0,0 +1,34 @@
+import { AppProcess } from "$ts/apps/process";
+import { AccountIcon } from "$ts/images/general";
+import type { App } from "$types/app";
+import ChooseProfilePicture from "./ChooseProfilePicture.svelte";
+
+export const ChooseProfilePictureApp: App = {
+  metadata: {
+    name: "Choose Profile Picture",
+    author: "Izaak Kuipers",
+    version: "1.0.0",
+    icon: AccountIcon,
+  },
+  position: { centered: true },
+  size: { w: 348, h: 416 },
+  minSize: { w: 348, h: 416 },
+  maxSize: { w: 348, h: 416 },
+  state: {
+    minimized: false,
+    maximized: false,
+    resizable: false,
+    headless: true,
+    fullscreen: false,
+  },
+  controls: {
+    minimize: false,
+    maximize: false,
+    close: true,
+  },
+  assets: {
+    runtime: AppProcess,
+    component: ChooseProfilePicture as any,
+  },
+  id: "FirstRun_ChooseProfilePicture",
+};
