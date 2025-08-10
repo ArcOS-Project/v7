296dec8ac015238833610da060e7b0ecc001316d
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-08-10 21:37:06 +0200

    Change tsparticles import

--- src/apps/user/qlorb/Qlorb/Main/Background.svelte
+++ src/apps/user/qlorb/Qlorb/Main/Background.svelte
@@ -1,25 +1,30 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import { tsParticles } from "tsparticles-engine";
-  import { loadFirePreset } from "tsparticles-preset-fire";
+  import { tsParticles } from "@tsparticles/engine";
+  import { loadFirePreset } from "@tsparticles/preset-fire";
 
   onMount(async () => {
     await loadFirePreset(tsParticles);
-    await tsParticles.load("__qlorb-bg", {
-      preset: "fire",
-      background: {
-        opacity: 0,
-      },
-      backgroundMode: false,
-      particles: {
-        number: {
-          limit: 500,
+    await tsParticles.load({
+      id: "__qlorb-bg",
+      options: {
+        preset: "fire",
+        background: {
+          opacity: 0,
         },
-        color: {
-          value: "#888",
-        },
-        opacity: {
-          value: 0.15,
+        backgroundMode: false,
+        particles: {
+          number: {
+            limit: {
+              value: 500,
+            },
+          },
+          color: {
+            value: "#888",
+          },
+          opacity: {
+            value: 0.15,
+          },
         },
       },
     });
