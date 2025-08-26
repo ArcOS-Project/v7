b2d5ebabc3a58da5fcc2e59f70c7a5b576815641
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-08-26 10:28:47 +0200

    Remove NotImplemented from UserLink

--- src/lib/UserLink.svelte
+++ src/lib/UserLink.svelte
@@ -1,26 +1,13 @@
 <script lang="ts">
-  import { MessageBox } from "$ts/dialog";
   import { DefaultIcon } from "$ts/images/apps";
   import type { PublicUserInfo } from "$types/user";
   import ProfilePicture from "./ProfilePicture.svelte";
 
   const { user, fallback = "", noPfp = false }: { user: PublicUserInfo; fallback?: string; noPfp?: boolean } = $props();
-
-  function dialog() {
-    MessageBox(
-      {
-        title: new URL(import.meta.url).pathname,
-        message: "Not implemented",
-        image: DefaultIcon,
-        buttons: [{ caption: "Okay", action: () => {}, suggested: true }],
-      },
-      2,
-    );
-  }
 </script>
 
 {#if user.displayName || user.username || fallback}
-  <button class="link user-link" onclick={dialog} title={user.username || fallback}>
+  <button class="link user-link" title={user.username || fallback}>
     {#if !noPfp}
       <ProfilePicture height={14} fallback={user.profilePicture} />
     {/if}
