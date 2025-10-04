86ba196bb3b285118604adf8e7d38d4bcb6c3fd9
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-10-04 12:08:42 +0200

    Organize calendar component

--- src/apps/components/shell/Shell/Taskbar/SystemArea/Calendar.svelte
+++ src/apps/components/shell/Shell/Taskbar/SystemArea/Calendar.svelte
@@ -4,88 +4,40 @@
   import Spinner from "$lib/Spinner.svelte";
   import dayjs, { Dayjs } from "dayjs";
   import weekOfYear from "dayjs/plugin/weekOfYear";
-  import { onMount } from "svelte";
+  import Top from "./Calendar/Top.svelte";
+  import Controls from "./Calendar/Controls.svelte";
+  import Header from "./Calendar/Header.svelte";
+  import Day from "./Calendar/Day.svelte";
 
   const { process }: { process: ShellRuntime } = $props();
   const { calendarOpened, userPreferences } = process;
 
   dayjs.extend(weekOfYear);
 
-  let timeStr = $state("");
-  let dateStr = $state("");
-  let weekNumber = $state(0);
   let date = $state<Dayjs>(dayjs().date(1));
   let month = $state<CalendarMonth>(process.getCalendarMonth());
 
   function update() {
     month = process.getCalendarMonth(date.format("YYYY-MM-DD"));
   }
-
-  function previousMonth() {
-    date = date.subtract(1, "month");
-    update();
-  }
-
-  function nextMonth() {
-    date = date.add(1, "month");
-    update();
-  }
-
-  function reset() {
-    date = dayjs().date(1);
-
-    update();
-  }
-
-  onMount(() => {
-    setInterval(() => {
-      timeStr = dayjs().format(`HH:mm`);
-      dateStr = dayjs().format(`D MMMM`);
-      weekNumber = dayjs().week();
-    }, 500);
-  });
 </script>
 
 <div class="calendar-popup shell-colored" class:opened={$calendarOpened} class:colored={$userPreferences.shell.taskbar.colored}>
-  <div class="top">
-    <h1 class="time">{timeStr}</h1>
-    <div>
-      <h1 class="date">{dateStr}</h1>
-      <p class="week">Week {weekNumber}</p>
-    </div>
-  </div>
+  <Top />
   <div class="bottom">
     {#if month}
-      <div class="controls">
-        <p class="selected-date">{date.format("MMMM YYYY")}</p>
-        <div class="buttons">
-          <button class="lucide icon-arrow-left" aria-label="Previous month" title="Previous month" onclick={previousMonth}
-          ></button>
-          <button class="today" onclick={reset}>Today</button>
-          <button class="lucide icon-arrow-right" aria-label="Next month" title="Next month" onclick={nextMonth}></button>
-        </div>
-      </div>
+      <Controls bind:date {update} />
       <div class="calendar">
-        <div class="header">
-          {#each shortWeekDays as day}
-            <div class="day">{day}</div>
-          {/each}
-        </div>
+        <Header />
         <div class="grid">
-          {#each month?.prepended as day}
-            <div class="day past" class:today={day.isToday}>
-              <p class="day-number">{day.dayOfMonth}</p>
-            </div>
+          {#each month?.prepended as day (day.fullDate)}
+            <Day past {day} />
           {/each}
-          {#each month?.current as day}
-            <div class="day" class:today={day.isToday}>
-              <p class="day-number">{day.dayOfMonth}</p>
-            </div>
+          {#each month?.current as day (day.fullDate)}
+            <Day {day} />
           {/each}
-          {#each month?.appended as day}
-            <div class="day future" class:today={day.isToday}>
-              <p class="day-number">{day.dayOfMonth}</p>
-            </div>
+          {#each month?.appended as day (day.fullDate)}
+            <Day future {day} />
           {/each}
         </div>
       </div>
---
+++ src/apps/components/shell/Shell/Taskbar/SystemArea/Calendar/Top.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+  import dayjs from "dayjs";
+  import { onMount } from "svelte";
+
+  let timeStr = $state("");
+  let dateStr = $state("");
+  let weekNumber = $state(0);
+
+  onMount(() => {
+    setInterval(() => {
+      timeStr = dayjs().format(`HH:mm`);
+      dateStr = dayjs().format(`D MMMM`);
+      weekNumber = dayjs().week();
+    }, 500);
+  });
+</script>
+
+<div class="top">
+  <h1 class="time">{timeStr}</h1>
+  <div>
+    <h1 class="date">{dateStr}</h1>
+    <p class="week">Week {weekNumber}</p>
+  </div>
+</div>
