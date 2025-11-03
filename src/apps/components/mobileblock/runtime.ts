26813e38668196a6dd3a1d202d51eb7f060ea650
Izaak Kuipers <izaak.kuipers@gmail.com>
2025-11-03 21:52:04 +0100

    MobileBlock

---
+++ src/apps/components/mobileblock/runtime.ts
@@ -0,0 +1,25 @@
+import { AppProcess } from "$ts/apps/process";
+import { IsMobile } from "$ts/env";
+import type { AppProcessData } from "$types/app";
+
+export class MobileBlockRuntime extends AppProcess {
+  public _criticalProcess: boolean = true;
+  //#region LIFECYCLE
+
+  constructor(pid: number, parentPid: number, app: AppProcessData) {
+    super(pid, parentPid, app);
+
+    this.setSource(__SOURCE__)
+  }
+
+  async start() {
+    const match = window.matchMedia("(max-width: 1000px)");
+    IsMobile.set(match.matches);
+
+    match.addEventListener("change", ({ matches }) => {
+      IsMobile.set(matches);
+    });
+  }
+
+  //#endregion
+}
