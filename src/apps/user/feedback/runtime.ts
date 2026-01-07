9511842b2e4bb1fa669f3e57653432e4b673a3f7
Izaak Kuipers <izaak.kuipers@gmail.com>
2026-01-07 14:57:17 +0100

    Lifecycle in FeedbackProcess

--- src/apps/user/feedback/runtime.ts
+++ src/apps/user/feedback/runtime.ts
@@ -3,11 +3,17 @@ import { Daemon } from "$ts/server/user/daemon";
 import type { AppProcessData } from "$types/app";
 
 export class FeedbackProcess extends Process {
+  //#region LIFECYCLE
+    
   constructor(pid: number, parentPid: number, app: AppProcessData) {
     super(pid, parentPid);
+
+    this.setSource(__SOURCE__);
   }
 
   async start() {
     Daemon.helpers?.iHaveFeedback(Daemon.getShell()!);
   }
+
+  //#endregion
 }
