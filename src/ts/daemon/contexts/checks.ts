6d5a341af93c82cc99037780635656d7fff3af18
Izaak Kuipers <izaak.kuipers@gmail.com>
2026-03-01 19:50:54 +0100

    fixes

--- src/ts/daemon/contexts/checks.ts
+++ src/ts/daemon/contexts/checks.ts
@@ -62,8 +62,6 @@ export class ChecksUserContext extends UserContext implements IChecksUserContext
     const distrib = this.serviceHost?.getService<DistributionServiceProcess>("DistribSvc");
     const updates = await distrib?.checkForAllStoreItemUpdates();
 
-    console.log(updates);
-
     if (updates?.length) {
       const first = updates[0];
       const notif = Daemon!.notifications?.sendNotification({
