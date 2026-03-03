44cca7faee4c90b49caaff6d91c82fd2cc81fa0a
Izaak Kuipers <izaak.kuipers@gmail.com>
2026-03-03 21:29:37 +0100

    Use CommandResult for publishing_updateStoreItemFromPath

--- src/interfaces/services/DistribSvc.ts
+++ src/interfaces/services/DistribSvc.ts
@@ -1,7 +1,9 @@
 import type { Constructs } from "$interfaces/common";
 import type { IInstallerProcessBase } from "$interfaces/distrib";
 import type { IBaseService } from "$interfaces/service";
+import type { CommandResult } from "$ts/result";
 import type { FilesystemProgressCallback } from "$types/fs";
+import type { UpdateWriteOpResult } from "$types/mongo";
 import type { ArcPackage, PartialStoreItem, StoreItem, UpdateInfo } from "$types/package";
 import type { UserPreferencesStore } from "$types/user";
 import type JSZip from "jszip";
@@ -60,5 +62,5 @@ export interface IDistributionServiceProcess extends IBaseService {
     itemId: string,
     updatePath: string,
     onProgress?: FilesystemProgressCallback
-  ): Promise<boolean>;
+  ): Promise<CommandResult<UpdateWriteOpResult>>;
 }
