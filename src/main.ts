1a8da594098099461a2f3e09ed7f29dfc7f30684
Izaak Kuipers <izaak.kuipers@gmail.com>
2026-03-04 12:48:55 +0100

    Remove that main.ts test thing

--- src/main.ts
+++ src/main.ts
@@ -1,22 +1,8 @@
-import type { Constructs } from "$interfaces/common";
-import type { IWaveKernel } from "$interfaces/kernel";
 import "./css/main.css";
 
 // CODE EXECUTION STARTS HERE
 async function Main() {
-  const mod = new URLSearchParams(location.search).get("kernel");
-
-  let WaveKernel: Constructs<IWaveKernel> | undefined;
-
-  if (!mod) {
-    const { WaveKernel: W } = await import("$ts/kernel/wavekernel");
-    WaveKernel = W;
-  } else {
-    const { WaveKernel: W } = await import(/* @vite-ignore */ mod);
-    WaveKernel = W;
-  }
-
-  if (!WaveKernel) return;
+  const { WaveKernel } = await import("$ts/kernel/wavekernel");
 
   const kernel = new WaveKernel();
 
