05bca42e93d780d3c12e89f8d1a2d6b5eef3a48e
Izaak Kuipers <izaak.kuipers@gmail.com>
2026-06-14 17:24:12 +0200

    Organize basic engine methods

--- src/ts/basic/engine/index.ts
+++ src/ts/basic/engine/index.ts
@@ -33,6 +33,25 @@ export class ArcBasicEngine {
     return this.programCounter > this.source.length - 1;
   }
 
+  constructor(source: string, config: BasicLang.Config, filename?: string) {
+    this.Log(`Constructing v${config.version}`);
+
+    this.source = source.split("\n").map((s) => s.replaceAll(/[\t\r]/g, ""));
+    this.stdin = config.stdin;
+    this.stdout = config.stdout;
+    this.stderr = config.stderr;
+    this.version = config.version;
+    this.DEBUG = config.debug ?? false;
+
+    if (filename) this.filename = filename;
+    if (config.builtinVariables) this.variables = config.builtinVariables;
+    if (config.functions) this.functions = config.functions;
+    if (config.commands) this.commands = config.commands;
+    if (config.slowdown) this.slowdown = config.slowdown;
+  }
+
+  //#region UTILS
+
   private Log(message: string, level = LogLevel.info) {
     Log(`ArcBasicEngine`, message, level);
   }
@@ -58,21 +77,8 @@ export class ArcBasicEngine {
     this.stderr(message);
   }
 
-  constructor(source: string, config: BasicLang.Config, filename?: string) {
-    this.Log(`Constructing v${config.version}`);
-
-    this.source = source.split("\n").map((s) => s.replaceAll(/[\t\r]/g, ""));
-    this.stdin = config.stdin;
-    this.stdout = config.stdout;
-    this.stderr = config.stderr;
-    this.version = config.version;
-    this.DEBUG = config.debug ?? false;
-    if (filename) this.filename = filename;
-    if (config.builtinVariables) this.variables = config.builtinVariables;
-    if (config.functions) this.functions = config.functions;
-    if (config.commands) this.commands = config.commands;
-    if (config.slowdown) this.slowdown = config.slowdown;
-  }
+  //#endregion
+  //#region EXECUTION
 
   async execute() {
     if (this.filename) {
@@ -113,71 +119,8 @@ export class ArcBasicEngine {
     if (result !== undefined) this.error(result, true);
   }
 
-  async getStrings(input: string, replaceVars = false): Promise<string[]> {
-    this.Debug(`getStrings >>${input}<< replaceVars=${replaceVars}`);
-
-    const strings = [...input.matchAll(REGEXES.STRING)].map((s) => s.groups?.str || "");
-
-    if (!strings.length) strings.push(input);
-
-    if (!replaceVars) return strings;
-
-    return await Promise.all(strings.map((s) => this.replaceVariables(s)));
-  }
-
-  async replaceVariables(input: string, runFunctions = true): Promise<string> {
-    this.Debug(`replaceVariables >>${input}<< runFunctions=${runFunctions}`);
-
-    if (!input || typeof input !== "string") return input;
-    const getters = [...input.matchAll(REGEXES.VARGET)];
-
-    for (const getter of getters) {
-      const { name, idx, hierarchy } = getter.groups ?? {};
-      if (!name) continue;
-
-      let original = `$${name}`;
-      let value: string;
-
-      if (idx !== undefined) original += `[${idx}]`;
-      else if (hierarchy) original += `{${hierarchy}}`;
-
-      if (idx !== undefined) value = await this.getFromArray(name, idx);
-      else if (hierarchy) value = await this.getFromObject(name, hierarchy);
-      else value = await this.getVariable(name);
-
-      input = input.replace(original, value);
-    }
-
-    if (runFunctions) input = await this.runFunctions(input);
-
-    return input;
-  }
-
-  async runFunctions(input: string) {
-    this.Debug(`runFunctions >>${input}<<`);
-
-    if (!input) return input;
-
-    const matches = [...input.matchAll(REGEXES.FUNCTION)];
-
-    if (!matches.length) return input;
-    if (input.match(/^"(.+)"$/)) input = input.replace(/^"(.+)"$/, "$1");
-
-    for (const match of matches) {
-      const { name, val } = match?.groups ?? {};
-      const func = name ? this.functions[name.toLowerCase()] : undefined;
-
-      if (!func) continue;
-
-      const newVal = await func(await this.replaceVariables(tryJsonParse(val)), this);
-
-      input = input.replace(`${name}(${val})`, typeof newVal !== "string" ? tryJsonStringify(newVal, 0) : newVal);
-    }
-
-    this.Debug(`runFunctions RESULT >>${input}<<`);
-
-    return input;
-  }
+  //#endregion
+  //#region VARIABLES
 
   async getVariable(key: string): Promise<string> {
     this.Debug(`getVariable >>${key}<<`);
@@ -211,6 +154,37 @@ export class ArcBasicEngine {
 
     this.variables[key.toLowerCase()] = async () => value;
   }
+
+  async replaceVariables(input: string, runFunctions = true): Promise<string> {
+    this.Debug(`replaceVariables >>${input}<< runFunctions=${runFunctions}`);
+
+    if (!input || typeof input !== "string") return input;
+    const getters = [...input.matchAll(REGEXES.VARGET)];
+
+    for (const getter of getters) {
+      const { name, idx, hierarchy } = getter.groups ?? {};
+      if (!name) continue;
+
+      let original = `$${name}`;
+      let value: string;
+
+      if (idx !== undefined) original += `[${idx}]`;
+      else if (hierarchy) original += `{${hierarchy}}`;
+
+      if (idx !== undefined) value = await this.getFromArray(name, idx);
+      else if (hierarchy) value = await this.getFromObject(name, hierarchy);
+      else value = await this.getVariable(name);
+
+      input = input.replace(original, value);
+    }
+
+    if (runFunctions) input = await this.runFunctions(input);
+
+    return input;
+  }
+
+  //#endregion
+  //#region ARRAYS
 
   async createArray(key: string, value: any[] = []) {
     this.Debug(`createArray >>${key}<<`);
@@ -277,6 +251,9 @@ export class ArcBasicEngine {
     }
   }
 
+  //#endregion
+  //#region OBJECTS
+
   async assignToObject(key: string, rawHierarchy: string, rawValue: string) {
     const variable = tryJsonParse((await this.getVariable(key)) ?? "");
 
@@ -301,41 +278,8 @@ export class ArcBasicEngine {
     return typeof value === "object" ? tryJsonStringify(value, 0) : value;
   }
 
-  async error(message: string, exit = false, columnIndexHint?: string) {
-    this.Debug(`error: >>${message}<< exit=${exit}`);
-
-    await this.setVariable("ERR", message);
-
-    if (exit) {
-      const line = this.source[this.programCounter];
-      const lineNumber = `${this.programCounter + 1}`;
-      const columnNumber = Math.max(columnIndexHint ? line.indexOf(columnIndexHint) : 0, 0);
-      const linePrefix = " ".repeat(lineNumber.length) + "  ║  ";
-      const leftOffset = " ".repeat(columnNumber);
-
-      this.sendToStderr(`\nError in ${this.filename}:${lineNumber}:${columnNumber} - ${message}\n`);
-      this.sendToStderr(`\n${linePrefix}`);
-      this.sendToStderr(`\n ${lineNumber} ║  ${line}`);
-      this.sendToStderr(`\n${linePrefix}${leftOffset}│`);
-      this.sendToStderr(`\n${linePrefix}${leftOffset}└─ ${message} (${lineNumber}:${columnNumber})`);
-      this.sendToStderr(`\n`);
-
-      for (const { name, line, type } of this.stackFrames.reverse()) {
-        this.sendToStderr(`\n  at ${type} ${name} (line ${line + 1})`);
-      }
-
-      this.sendToStderr(`\n`);
-      this.jumpEnd();
-    }
-  }
-
-  captureStackFrame(name: string, type: BasicLang.BasicStackFrameType) {
-    this.stackFrames.push({
-      line: this.programCounter,
-      name,
-      type,
-    });
-  }
+  //#endregion
+  //#region SUBROUTINES
 
   async createSubRoutine(name: string, startIdx: number = this.programCounter) {
     this.Debug(`createSubRoutine >>${name}<< startIdx=${startIdx}`);
@@ -370,6 +314,47 @@ export class ArcBasicEngine {
     this.jump(returnTo);
   }
 
+  //#endregion
+  //#region PARSING
+
+  async runFunctions(input: string) {
+    this.Debug(`runFunctions >>${input}<<`);
+
+    if (!input) return input;
+
+    const matches = [...input.matchAll(REGEXES.FUNCTION)];
+
+    if (!matches.length) return input;
+    if (input.match(/^"(.+)"$/)) input = input.replace(/^"(.+)"$/, "$1");
+
+    for (const match of matches) {
+      const { name, val } = match?.groups ?? {};
+      const func = name ? this.functions[name.toLowerCase()] : undefined;
+
+      if (!func) continue;
+
+      const newVal = await func(await this.replaceVariables(tryJsonParse(val)), this);
+
+      input = input.replace(`${name}(${val})`, typeof newVal !== "string" ? tryJsonStringify(newVal, 0) : newVal);
+    }
+
+    this.Debug(`runFunctions RESULT >>${input}<<`);
+
+    return input;
+  }
+
+  async getStrings(input: string, replaceVars = false): Promise<string[]> {
+    this.Debug(`getStrings >>${input}<< replaceVars=${replaceVars}`);
+
+    const strings = [...input.matchAll(REGEXES.STRING)].map((s) => s.groups?.str || "");
+
+    if (!strings.length) strings.push(input);
+
+    if (!replaceVars) return strings;
+
+    return await Promise.all(strings.map((s) => this.replaceVariables(s)));
+  }
+
   async expression(input: string) {
     this.Debug(`expression >>${input}<<`);
 
@@ -399,6 +384,9 @@ export class ArcBasicEngine {
     await this.error(`Unknown expression '${mode}'`, true, mode);
     return false;
   }
+
+  //#endregion
+  //#region JUMPING
 
   async findNextEndling(endling: string, startIdx = this.programCounter): Promise<number | undefined> {
     this.Debug(`findNextEndling >>${endling}<< startIdx=${startIdx}`);
@@ -431,6 +419,48 @@ export class ArcBasicEngine {
     this.HALT = true;
     this.jump(this.source.length);
   }
+
+  //#endregion
+  //#region ERROR HANDLING
+
+  async error(message: string, exit = false, columnIndexHint?: string) {
+    this.Debug(`error: >>${message}<< exit=${exit}`);
+    if (exit) this.jumped = true;
+
+    await this.setVariable("ERR", message);
+
+    if (exit) {
+      const line = this.source[this.programCounter];
+      const lineNumber = `${this.programCounter + 1}`;
+      const columnNumber = Math.max(columnIndexHint ? line.indexOf(columnIndexHint) : 0, 0);
+      const linePrefix = " ".repeat(lineNumber.length) + "  ║  ";
+      const leftOffset = " ".repeat(columnNumber);
+
+      this.sendToStderr(`\nError in ${this.filename}:${lineNumber}:${columnNumber} - ${message}\n`);
+      this.sendToStderr(`\n${linePrefix}`);
+      this.sendToStderr(`\n ${lineNumber} ║  ${line}`);
+      this.sendToStderr(`\n${linePrefix}${leftOffset}│`);
+      this.sendToStderr(`\n${linePrefix}${leftOffset}└─ ${message} (${lineNumber}:${columnNumber})`);
+      this.sendToStderr(`\n`);
+
+      for (const { name, line, type } of this.stackFrames.reverse()) {
+        this.sendToStderr(`\n  at ${type} ${name} (line ${line + 1})`);
+      }
+
+      this.sendToStderr(`\n`);
+      this.jumpEnd();
+    }
+  }
+
+  captureStackFrame(name: string, type: BasicLang.BasicStackFrameType) {
+    this.stackFrames.push({
+      line: this.programCounter,
+      name,
+      type,
+    });
+  }
+
+  //#endregion
 
   static async FromSource(relativePath: string, config: BasicLang.Config) {
     if (!config.readScriptFile) {
