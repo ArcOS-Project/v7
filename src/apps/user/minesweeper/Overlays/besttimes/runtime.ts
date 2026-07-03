6222fd17b1cbc40c4f92b46512c5d050c00a43b4
Logan Cookson <66440439+logangamesdaily@users.noreply.github.com>
2026-07-03 08:44:07 +0300

    Change import to type for IAppProcess

--- src/apps/user/minesweeper/Overlays/besttimes/runtime.ts
+++ src/apps/user/minesweeper/Overlays/besttimes/runtime.ts
@@ -2,7 +2,7 @@ import { AppProcess } from "$ts/apps/process";
 import type { AppProcessData } from "$types/apps/app";
 import type { MinesweeperRuntime } from "../../runtime";
 import { DefaultMinesweeperConfiguration } from "../../store";
-import { IAppProcess } from "$interfaces/IAppProcess";
+import type { IAppProcess } from "$interfaces/IAppProcess";
 
 export class MinesweeperBestTimesRuntime extends AppProcess implements IAppProcess {
   parent: MinesweeperRuntime;
